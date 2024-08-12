7e97cecf814a6422dc7ce0a4016bd91ca83f4cdb
Ralph Debusmann <ralph.debusmann@mgb.ch>
2024-08-12 17:55:03 +0200

    0.0.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='pgrestpy',
-    version='0.0.6',
+    version='0.0.7',
     description='Simple REST client for PostgreSQL-compatible databases',
     long_description=long_description,
     long_description_content_type='text/markdown',
